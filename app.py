e41da10c415f9823aff3bc8981b24a5c212c1a20
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-04-08 21:09:41 +0530

    rendering template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello People"
+    return render_template("login.html")
+    
 
 if __name__ == "__main__":
     app.run(debug = True, port=8900)
