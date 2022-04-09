37aba6cf0599267019d2b11d08200077ef75136b
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-04-09 21:01:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from distutils.log import debug
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLAlchemy_DATABASE_URI'] = "sqlite:///track-it.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
